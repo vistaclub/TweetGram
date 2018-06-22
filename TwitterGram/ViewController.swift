0867367e1444ddc635fb3dae9811e2473bacc1df
Jason Wong <jawong71@gmail.com>
2018-06-22 15:53:33 -0700

    Initial Commit

---
+++ TwitterGram/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  TwitterGram
+//
+//  Created by Jason Wong on 2018-06-22.
+//  Copyright © 2018 Jason Wong. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
